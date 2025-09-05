f396b9a45025da19061672e9d42c10050c7b2363
Grifin10 <121900538+Grifin10@users.noreply.github.com>
2025-09-05 11:54:44 -0300

    Added Popup window functionality

--- app/contador.tsx
+++ app/contador.tsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 export default function Index() {
 const [counter, setCounter] = useState({value: 0});
 
-const increment = (prev) => {
+const aumentar = (prev) => {
     setCounter({value: prev.value + 1});
 };
 
-const decrease = (prev) => {
+const reducir = (prev) => {
     setCounter({value: prev.value - 1});
 };
 
@@ -16,39 +16,44 @@ return (
     <View style={styles.screenFlex}>
 
     <View style={styles.buttonFlex}>
-    <Pressable onPress={() => increment(counter)}>
-        <View><Text style={styles.button}>Increment counter</Text> </View>
+    <Pressable onPress={() => reducir(counter)}>
+        <View><Text style={styles.button}>Reducir contador</Text> </View>
     </Pressable>
 
-    <Pressable onPress={() => decrease(counter)}>
-        <View><Text style={styles.button}>Decrease counter</Text> </View>
+    <Pressable onPress={() => aumentar(counter)}>
+        <View><Text style={styles.button}>Aumentar contador</Text> </View>
     </Pressable>
     </View>
 
-    <Text>Counter value: {counter.value}</Text>
+    <Text style={[{fontSize:18}]}>Valor actual: {counter.value}</Text>
 
     </View>
 );
 }
 
 const styles = StyleSheet.create({
-screenFlex: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center", 
-},
+    screenFlex: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center", 
+    },
 
-buttonFlex: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center", 
-},
+    buttonFlex: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center", 
+    },
 
-button: {
-    color: "#2478ffff",
-    fontSize: 18,
-    fontFamily: "SF Pro Text",
-    padding: 10,
-},
+    button: {
+        color: "#000000ff",
+        backgroundColor: "#ff0000ff",
+        fontSize: 18,
+        fontFamily: "SF Pro Text",
+        padding: 10,
+        marginTop: 20,
+        marginBottom: 14,
+        marginHorizontal: 10,
+        borderRadius: 26
+    },
 });
--- app/perfil.tsx
+++ app/perfil.tsx
@@ -1,46 +1,57 @@
 import { Text, View, Pressable, StyleSheet } from "react-native";
 import { useState } from "react";
+import DialogInput from "react-native-dialog-input";;
 
 export default function Index() {
     const [nombre, setNombre] = useState({nombre: "Facundo del Valle"});
-
-    const cambiarNombre = () => {
-        setNombre({nombre: "pepe"})
-    }
+    const [visible, setVisible] = useState<boolean>(false);
 
     return (
         <View style={styles.screenFlex}>
             <Text style={[{fontSize: 22}]}>{nombre.nombre}</Text>
             <View style={styles.buttonFlex}>
-                <Pressable onPress={cambiarNombre}>
+                <Pressable onPress={(() => setVisible(true))}>
                     <View><Text style={styles.button}>Cambiar nombre</Text> </View>
                 </Pressable>
+                <DialogInput 
+                    isDialogVisible={visible}
+                    title={"Ingrese su nombre"}
+                    message={"Por favor ingrese su nombre"}
+                    hintInput ={"Escriba aqui..."}
+                    submitInput={ (inputText) => {
+                        setNombre({nombre: inputText});
+                        setVisible(false);
+                    }}
+                    closeDialog={ () => {setVisible(false)}}
+                    submitText={"Aceptar"}
+                    cancelText={"Cancelar"}
+                />
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-screenFlex: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center", 
-},
+    screenFlex: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center", 
+    },
 
-buttonFlex: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center", 
-},
+    buttonFlex: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center", 
+    },
 
-button: {
-    color: "#000000ff",
-    backgroundColor: "#ff0000ff",
-    fontSize: 18,
-    fontFamily: "SF Pro Text",
-    padding: 10,
-    marginTop: 20,
-    borderRadius: 26
-},
+    button: {
+        color: "#000000ff",
+        backgroundColor: "#ff0000ff",
+        fontSize: 18,
+        fontFamily: "SF Pro Text",
+        padding: 10,
+        marginTop: 20,
+        borderRadius: 26
+    },
 });
