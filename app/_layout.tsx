3630d1166ead2ea21ec57e2912a6a1d3f459dc29
Grifin10 <121900538+Grifin10@users.noreply.github.com>
2025-09-13 20:14:00 -0300

    refactor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout() {
       }}
     />
     <Tabs.Screen
-      name="profile"
+      name="index"
       options={{
         title: "Profile",
         tabBarIcon: ({ color }) => <FontAwsome name="user" size={25}/>
--- app/cards.tsx
+++ app/cards.tsx
@@ -1,5 +1,4 @@
 import {StyleSheet, FlatList, View } from "react-native";
-import React from "react";
 import Tarjeta from "@/components/Tarjeta";
 
 // Conjunto de tarjetas a representar
