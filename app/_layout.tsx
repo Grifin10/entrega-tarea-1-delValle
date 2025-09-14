b0ec7be1fe600b73462859dcb001a109e3775ec9
Grifin10 <121900538+Grifin10@users.noreply.github.com>
2025-09-14 16:26:03 -0300

    added gallery tab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,5 +25,12 @@ export default function RootLayout() {
         tabBarIcon: ({ color }) => <FontAwsome name="hashtag" size={25}/>
       }}
     />
+      <Tabs.Screen
+      name="gallery"
+      options={{
+        title: "Gallery",
+        tabBarIcon: ({ color }) => <FontAwsome name="table" size={25}/>
+      }}
+    />
   </Tabs>);
 }
---
+++ app/gallery.tsx
@@ -0,0 +1,35 @@
+import {StyleSheet, FlatList, View } from "react-native";
+import React from "react";
+import Tarjeta from "@/components/Tarjeta";
+
+// Conjunto de tarjetas a representar
+const cards = [
+  {id: "1", title: "FORZA"},
+  {id: "2", title: "FERRARI"},
+  {id: "3", title: "LEC16"},
+  {id: "4", title: "HAM44" },
+];
+
+export default function Index(){
+  // Se retorna una FlatList que para cada tarjeta en cards llama a renderItem y ahi se crea un Item con los parametros correspondientes
+  return (
+      <View style = {styles.container}>
+        <FlatList
+          data = {cards}
+          renderItem = {({item}) => (<Tarjeta{...item}/>)}
+          keyExtractor = {item => item.id}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center", 
+          }}
+        />
+      </View>
+  )
+}
+
+// Estilos para los diferentes elementos
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
